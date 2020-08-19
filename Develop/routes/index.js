f37bad73c949cfb5de48a279a613ed661eab257d
Frank Carr <gundamtrooperx@gmail.com>
2020-08-19 15:32:15 -0700

    added post routes and delete routes

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -19,4 +19,37 @@ router.get("/api/notes", function(req, res){
     res.json(db)
 })
 
+router.post("/api/notes", function(req, res){
+    let recall = {
+        id: Math.floor(Math.random() * 100), 
+        title: req.body.title, 
+        text: req.body.text
+    }
+    db.push(recall)
+    console.log("db")
+    fs.writeFileSync("./db/db.json",JSON.stringify(db), function(){
+        
+        console.log("post Route", db)
+        res.json(db)
+    })
+    
+})
+
+router.delete("/api/notes", function(req, res){
+    var deletenotes = []
+    for(let i = 0; i < db.length; i ++ ){
+        if(db[i].id != req.params.id){
+            deletenotes.push(db[i])
+        }
+    }
+    db = deletenotes
+    fs.writeFileSync("./db/db.json",JSON.stringify(db), function(){
+        
+        console.log("delete Route", db)
+        res.json(db)
+    })
+    
+})
+
+
 module.exports = router
